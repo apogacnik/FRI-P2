3c346a85418acbb7ada5a05d4f3bf1bfd99ca620
apogacnik <anzy.pog@gmail.com>
2023-05-07 21:31:06 +0200

    Resil kolokvij2020_1 naloga1 in naloga2

--- kolokviji/kolokviji2020/kolokvij2020_1/naloga1/naloga1.c
+++ kolokviji/kolokviji2020/kolokvij2020_1/naloga1/naloga1.c
@@ -1,11 +1,26 @@
+// 10/10 javni
+// 50/50 skriti
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 
 int main() {
-    // dopolnite ...
+  int n;
+  scanf("%d", &n);
 
-    return 0;
+  int sumPredhodnih = 0;
+  int curr;
+  int d;
+  for (d = 0; d < n; d++) {
+    scanf("%d", &curr);
+
+    // povprecje = sumPredhodnik/d, oz. 0, ce d = 0
+    int povprecje = (d > 0) ? sumPredhodnih/d : 0;
+
+    if (curr <= povprecje) break;
+    sumPredhodnih += curr;
+  }
+
+  printf("%d\n", d);
+
+  return 0;
 }
--- kolokviji/kolokviji2020/kolokvij2020_1/naloga2/naloga2.c
+++ kolokviji/kolokviji2020/kolokvij2020_1/naloga2/naloga2.c
@@ -1,16 +1,45 @@
+// 10/10 javni
+// 50/50 skriti
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 
 #include "naloga2.h"
 
+// Vrne dolzino tabele (, ki se konca z 0)
+int getLength(int* t) {
+  int len;
+  for (len = 0; t[len] != 0; len++) {
+    continue;
+  }
+
+  return len + 1;
+}
+
+// Kopira tabelo orig v tabelo copy
+void kopiraj(int* orig, int* copy, int len) {
+  for (int i = 0; i < len; i++) {
+    copy[i] = orig[i];
+  }
+
+  return;
+}
+
 void pretvori(int** t) {
-    // dopolnite ...
+  while (*t != NULL) {
+    int currTabLen = getLength(*t);
+    int* copy = (int*)malloc(currTabLen * sizeof(int));
+
+    kopiraj(*t, copy, currTabLen);
+
+    *t = copy; // zapisi nov pointer v primarno tabelo
+    *t++;
+  }
+
+  return;
 }
 
 int main() {
-    // koda za ro"cno testiranje (po "zelji)
-    return 0;
+
+  return 0;
 }
